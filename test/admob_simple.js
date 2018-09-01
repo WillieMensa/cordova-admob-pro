39fe288046b5d7811499d6ae32a60c3986a65eb1
Willie Verger <ingverger@gmail.com>
2018-08-31 22:28:49 -0300

    willizacion 1.0.0
    willizacion 1.0.0

--- test/admob_simple.js
+++ test/admob_simple.js
@@ -3,20 +3,20 @@ var admobid = {};
 // TODO: replace the following ad units with your own
 if( /(android)/i.test(navigator.userAgent) ) {
   admobid = { // for Android
-    banner: 'ca-app-pub-3940256099942544/6300978111',
-    interstitial: 'ca-app-pub-3940256099942544/1033173712',
-    rewardvideo: 'ca-app-pub-3940256099942544/5224354917',
+    banner: 'ca-app-pub-4076082038101002/2921419225',
+    interstitial: 'ca-app-pub-4076082038101002/2949339385',
+    rewardvideo: '',
   };
 } else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
   admobid = { // for iOS
-    banner: 'ca-app-pub-3940256099942544/4480807092',
-    interstitial: 'ca-app-pub-3940256099942544/4411468910',
-    rewardvideo: 'ca-app-pub-3940256099942544/1712485313',
+    banner: 'ca-app-pub-4076082038101002/2921419225',
+    interstitial: 'ca-app-pub-4076082038101002/2949339385',
+    rewardvideo: '',
   };
 } else {
   admobid = { // for Windows Phone
-    banner: 'ca-app-pub-6869992474017983/8878394753',
-    interstitial: 'ca-app-pub-6869992474017983/1355127956',
+    banner: 'ca-app-pub-4076082038101002/2921419225',
+    interstitial: 'ca-app-pub-4076082038101002/2949339385',
     rewardvideo: '',
   };
 }
